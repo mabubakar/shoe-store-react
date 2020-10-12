8cdde044c304c4c820ab564aae0c2a4e54f5be88
Abubakar <chabubakar001@gmail.com>
2020-10-12 20:46:14 +0500

    12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 
 import About from './pages/AboutPage'
-import Card from './pages/CardPage'
+import Cart from './pages/CartPage'
 import Contact from './pages/ContactPage'
 import Default from './pages/Default'
 import Home from './pages/HomePage'
 import Products from './pages/Products'
 import SingleProduct from './pages/SingleProductPage'
 
+import { Switch, Route } from 'react-router-dom'
+
 const App = () => {
   return (
     <>
-      <h1>Abubakar</h1>
-      <h1>Muhammad</h1>
+      <Switch>
+        <Route exact path='/' components={Home} />
+        <Route path='/about' components={About} />
+        <Route path='/contact' components={Contact} />
+        <Route path='/cart' components={Cart} />
+        <Route path='/products' components={Products} />
+        <Route exact path='/products:id' components={SingleProduct} />
+        <Route components={Default} />
+      </Switch>
     </>
   )
 }

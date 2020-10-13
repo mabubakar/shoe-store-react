14466f6d468764e18c01a1cabd8dea5c496e8f5b
Abubakar <chabubakar001@gmail.com>
2020-10-13 16:57:31 +0500

    commit 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ProductProvider, ProductConsumer } from './context/context'
+import { ProductProvider } from './context/context'
 ReactDOM.render(
   <ProductProvider>
     <Router>
